c585d640f4b75e768e09c3e1375ce20b587e8a8c
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-16 16:04:34 +0600

    middle header is done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopHeader from './Top-Header';
+
+const Header = () => {
+    return (
+        <div className='container mx-auto'>
+            <TopHeader></TopHeader>
+        </div>
+    );
+};
+
+export default Header;
