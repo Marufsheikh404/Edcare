12f81165b68780e78f38f6384bb19cdce5accef5
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-19 23:05:29 +0600

    contact page is done

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -7,6 +7,12 @@ import { FaMap, FaStopwatch } from 'react-icons/fa6';
 import { FiChevronDown, FiMail, FiMessageSquare, FiUser } from 'react-icons/fi';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import banner from '../../images/banner/banner.png'
+import shadow from '../../images/shadow/10002.png'
+import cricel from '../../images/shadow/cricel.png'
+import cricel2 from '../../images/shadow/cricle2.png'
+import Link from 'next/link';
+import Image from 'next/image';
 
 
 const ContactPage = () => {
@@ -26,6 +32,67 @@ const ContactPage = () => {
     }
     return (
         <div>
+            <div className="relative">
+                <div className="relative w-full">
+                    <div className="relative w-full h-80 lg:h-100 ">
+                        <Image
+                            src={banner}
+                            alt="banner"
+                            fill
+                            priority
+                            style={{ objectFit: "cover" }}
+                            className="block"
+                        />
+                    </div>
+
+                    <Image
+                        src={shadow}
+                        alt="shadow"
+                        width={200}
+                        height={220}
+                        className="pointer-events-none absolute left-0 w-40 lg:w-50 lg:left-4 top-20  z-10"
+                    />
+
+                    <Image
+                        src={cricel}
+                        alt="cricel"
+                        width={120}
+                        height={140}
+                        className="pointer-events-none absolute left-1/3 top-67 lg:top-85   z-10"
+                    />
+
+                    <Image
+                        src={cricel2}
+                        alt="cricel2"
+                        width={160}
+                        height={160}
+                        className="pointer-events-none absolute right-0 top-0  z-10"
+                    />
+
+                    <div className="absolute top-30 left-40 lg:left-50 z-10">
+                        <div className="max-w-4xl bg-transparent">
+                            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-gray-900">
+                               Contact Us
+                            </h1>
+
+                            <div className="mt-3 flex flex-wrap items-center gap-2 text-base sm:text-lg">
+                                <h3 className="text-base sm:text-lg font-semibold">
+                                    <Link href="/" className="hover:text-[#2FA79B]">Home</Link>
+                                </h3>
+
+                                <span className="text-base sm:text-lg">/</span>
+
+                                <span className="text-base sm:text-lg font-semibold text-[#2FA79B]">
+                                    Contact Us
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute top-0 left-0 h-full w-4/5 pointer-events-none">
+                    <div className="h-full w-full bg-linear-to-r from-white to-transparent" />
+                </div>
+            </div>
             <section className="max-w-7xl mx-auto px-4 py-12 grid grid-cols-1 lg:grid-cols-2 gap-8">
 
 
