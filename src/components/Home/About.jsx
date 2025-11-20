ed5a50feb86f094c6f28a7bcffb2df07c340cd6c
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-20 22:45:28 +0600

    animation

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -22,39 +22,40 @@ export default function AboutPage() {
     }, [])
     return (
         <section className="w-full bg-white py-20 px-4 md:px-10">
-            <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
+            <div className="md:flex md:gap-6 md:items-stretch">
 
-                <div data-aos="fade-up-right" data-aos-duration="2000" className="md:col-span-3 flex flex-col items-center md:items-start md:-translate-y-13">
-                    <div className="border border-gray-300 p-2 rounded-tr-4xl rounded-bl-4xl w-full md:max-w-[250px]">
+                {/* Left Column */}
+                <div data-aos="fade-up-right" data-aos-duration="2000" className="md:flex-1 flex flex-col items-center md:items-start md:-translate-y-13">
+                    <div className="border border-gray-300 p-2 rounded-tr-4xl rounded-bl-4xl w-full max-w-[250px]">
                         <Image
-                            className="rounded-tr-4xl rounded-bl-4xl h-80 w-full"
+                            className="rounded-tr-4xl rounded-bl-4xl h-auto w-full"
                             src={image}
                             alt="image"
                         />
                     </div>
-                    <div className="bg-[#07A698] mt-3 flex items-center rounded-tr-4xl rounded-bl-4xl p-2 w-full h-25 max-w-[250px]">
+                    <div className="bg-[#07A698] mt-3 flex items-center rounded-tr-4xl rounded-bl-4xl p-2 w-full max-w-[250px] h-auto">
                         <div className="bg-white rounded-full p-4">
                             <FiPhoneCall />
                         </div>
                         <div className="flex flex-col items-start ml-3">
                             <p className="text-white text-md uppercase">Online Support</p>
-                            <h1 className="text-white text-2xl font-semibold">+258 152 3659</h1>
+                            <h1 className="text-white text-xl font-semibold">+258 152 3659</h1>
                         </div>
                     </div>
                 </div>
 
-
-                <div data-aos="fade-up-right" data-aos-duration="2000" className="md:col-span-3 flex justify-center md:justify-start mt-10 md:mt-0">
+                {/* Middle Column */}
+                <div data-aos="fade-up-right" data-aos-duration="2000" className="md:flex-1 flex justify-center md:justify-start mt-10 md:mt-0">
                     <Image
-                        className="rounded-tl-4xl rounded-br-4xl w-full h-96"
+                        className="rounded-tl-4xl rounded-br-4xl w-full  h-86"
                         src={image2}
                         alt="image2"
                     />
                 </div>
 
-
-                <div className="md:col-span-6 flex flex-col gap-5 mt-10 md:mt-0">
-                    <div data-aos="fade-up" data-aos-duration="3000" className="flex items-center gap-3 cursor-pointer bg-white shadow-lg p-1 border border-gray-200 w-full max-w-[300px] rounded-full  mx-auto md:mx-0">
+                {/* Right Column */}
+                <div className="md:flex-2 flex flex-col gap-5 mt-10 md:mt-0">
+                    <div data-aos="fade-up" data-aos-duration="3000" className="flex items-center gap-3 cursor-pointer bg-white shadow-lg p-1 border border-gray-200 w-full max-w-[300px] rounded-full mx-auto md:mx-0">
                         <div className="rounded-full shadow p-3">
                             <Image src={image3} width={30} height={30} alt="logo" />
                         </div>
@@ -73,10 +74,10 @@ export default function AboutPage() {
                     <div className="mt-3 flex flex-col sm:flex-row items-center justify-around gap-5">
                         <div data-aos="fade-up" data-aos-duration="2000" className="flex items-center gap-3">
                             <div className="bg-white shadow p-3 border border-gray-200 rounded-full">
-                                <Image src={tv} alt="" />
+                                <Image width={25} src={tv} alt="" />
                             </div>
                             <div>
-                                <h1 className="text-3xl font-bold text-[#07A698]">9.5+K</h1>
+                                <h1 className="text-xl font-bold text-[#07A698]">9.5+K</h1>
                                 <p className="ct text-center sm:text-left">
                                     Total active students <br />
                                     taking gifted courses
@@ -86,10 +87,10 @@ export default function AboutPage() {
 
                         <div data-aos="fade-up" data-aos-duration="2000" className="flex items-center gap-3">
                             <div className="bg-white shadow p-3 border border-gray-200 rounded-full">
-                                <Image src={ed} alt="" />
+                                <Image width={25} src={ed} alt="" />
                             </div>
                             <div>
-                                <h1 className="text-3xl font-bold text-[#07A698]">6.5+K</h1>
+                                <h1 className="text-xl font-bold text-[#07A698]">6.5+K</h1>
                                 <p className="ct text-center sm:text-left">
                                     Total active students <br />
                                     taking gifted courses
