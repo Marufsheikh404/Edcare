75d9da58eb643475c61a8815f7703734501514b7
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-21 21:21:46 +0600

    commit

--- src/app/details/[id]/page.js
+++ src/app/details/[id]/page.js
@@ -5,6 +5,11 @@ import Image from 'next/image';
 import { useParams } from 'next/navigation';
 import React, { useContext } from 'react';
 import { useDispatch } from 'react-redux';
+import banner from '../../../images/banner/banner.png'
+import shadow from '../../../images/shadow/10002.png'
+import cricel from '../../../images/shadow/cricel.png'
+import cricel2 from '../../../images/shadow/cricle2.png'
+import Link from 'next/link';
 
 const DetailsPage = () => {
     const dispatch = useDispatch();
@@ -13,69 +18,134 @@ const DetailsPage = () => {
     const findCourse = courses?.find(item => item?.id === Number(id));
 
     return (
-        <div className="shadow-md rounded-2xl overflow-hidden bg-[#E8EFF6] hover:shadow-2xl transition flex flex-col lg:flex-row items-center gap-4 p-4">
-            {/* Left: Image */}
-            <div className="w-full">
-                <Image
-                    width={200}
-                    height={200}
-                    src={findCourse?.thumbnail}
-                    alt={findCourse?.title || "image"}
-                    className="object-cover w-[450px] h-auto rounded-xl"
-                />
+        <div>
+
+            <div className="relative">
+                <div className="relative w-full">
+                    <div className="relative w-full h-80 lg:h-100 ">
+                        <Image
+                            src={banner}
+                            alt="banner"
+                            fill
+                            priority
+                            style={{ objectFit: "cover" }}
+                            className="block"
+                        />
+                    </div>
+
+                    <Image
+                        src={shadow}
+                        alt="shadow"
+                        width={200}
+                        height={220}
+                        className="pointer-events-none absolute left-0 w-40 lg:w-50 lg:left-4 top-20  z-10"
+                    />
+
+                    <Image
+                        src={cricel}
+                        alt="cricel"
+                        width={120}
+                        height={140}
+                        className="pointer-events-none absolute left-1/3 top-67 lg:top-85   z-10"
+                    />
+
+                    <Image
+                        src={cricel2}
+                        alt="cricel2"
+                        width={160}
+                        height={160}
+                        className="pointer-events-none absolute right-0 top-0  z-10"
+                    />
+
+                    <div className="absolute top-30 left-40 lg:left-50 z-10">
+                        <div className="max-w-4xl bg-transparent">
+                            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-gray-900">
+                                Details Page
+                            </h1>
+
+                            <div className="mt-3 flex flex-wrap items-center gap-2 text-base sm:text-lg">
+                                <h3 className="text-base sm:text-lg font-semibold">
+                                    <Link href="/" className="hover:text-[#2FA79B]">Home</Link>
+                                </h3>
+
+                                <span className="text-base sm:text-lg">/</span>
+
+                                <span className="text-base sm:text-lg font-semibold text-[#2FA79B]">
+                                    Details Page
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute top-0 left-0 h-full w-4/5 pointer-events-none">
+                    <div className="h-full w-full bg-linear-to-r from-white/80 to-transparent" />
+                </div>
             </div>
 
-
-            {/* Right: Content */}
-            <div className="p-4 flex flex-col gap-3 w-full h-full relative">
-                <p className="text-sm font-semibold text-[#07A698] bg-[#E6F6F4] rounded-full px-2 py-1 w-fit mb-1">
-                    {findCourse?.badge}
-                </p>
-
-
-                <h2 className="text-2xl font-bold leading-snug">{findCourse?.title}</h2>
-
-
-                <div className="flex items-center gap-4 text-gray-500 text-sm">
-                    <span className="font-semibold">Lessons: {findCourse?.lessons}</span>
-                    <span className="font-semibold">Students: {findCourse?.students}</span>
+            <div className="shadow-md rounded-2xl overflow-hidden bg-[#E8EFF6] hover:shadow-2xl transition flex flex-col lg:flex-row items-center gap-4 p-4">
+                {/* Left: Image */}
+                <div className="w-full">
+                    <Image
+                        width={200}
+                        height={200}
+                        src={findCourse?.thumbnail}
+                        alt={findCourse?.title || "image"}
+                        className="object-cover w-[450px] h-auto rounded-xl"
+                    />
                 </div>
 
 
-                <p className="text-sm text-gray-500 font-semibold">Views: {findCourse?.views}</p>
+                {/* Right: Content */}
+                <div className="p-4 flex flex-col gap-3 w-full h-full relative">
+                    <p className="text-sm font-semibold text-[#07A698] bg-[#E6F6F4] rounded-full px-2 py-1 w-fit mb-1">
+                        {findCourse?.badge}
+                    </p>
 
 
-                <div className="flex items-center gap-2">
-                    <div className="w-8 h-8 rounded-full overflow-hidden bg-gray-200">
-                        {findCourse?.instructor?.avatar && (
-                            <Image
-                                width={200}
-                                height={200}
-                                src={findCourse?.instructor?.avatar}
-                                alt={findCourse?.instructor?.name}
-                                className="object-cover w-full h-full"
-                            />
-                        )}
+                    <h2 className="text-2xl font-bold leading-snug">{findCourse?.title}</h2>
+
+
+                    <div className="flex items-center gap-4 text-gray-500 text-sm">
+                        <span className="font-semibold">Lessons: {findCourse?.lessons}</span>
+                        <span className="font-semibold">Students: {findCourse?.students}</span>
                     </div>
-                    <div>
-                        <p className="font-medium text-xs">{findCourse?.instructor?.name}</p>
-                        <p className="text-[10px] text-gray-500">{findCourse?.instructor?.role}</p>
+
+
+                    <p className="text-sm text-gray-500 font-semibold">Views: {findCourse?.views}</p>
+
+
+                    <div className="flex items-center gap-2">
+                        <div className="w-8 h-8 rounded-full overflow-hidden bg-gray-200">
+                            {findCourse?.instructor?.avatar && (
+                                <Image
+                                    width={200}
+                                    height={200}
+                                    src={findCourse?.instructor?.avatar}
+                                    alt={findCourse?.instructor?.name}
+                                    className="object-cover w-full h-full"
+                                />
+                            )}
+                        </div>
+                        <div>
+                            <p className="font-medium text-xs">{findCourse?.instructor?.name}</p>
+                            <p className="text-[10px] text-gray-500">{findCourse?.instructor?.role}</p>
+                        </div>
                     </div>
-                </div>
 
 
-                <div className="inline-block border-b border-gray-300 pb-1 text-sm">
-                    ⭐⭐⭐⭐⭐ {findCourse?.rating}k
-                </div>
+                    <div className="inline-block border-b border-gray-300 pb-1 text-sm">
+                        ⭐⭐⭐⭐⭐ {findCourse?.rating}k
+                    </div>
 
 
-                <p className="font-semibold text-gray-800 text-sm">Price: $ {findCourse?.price}</p>
+                    <p className="font-semibold text-gray-800 text-sm">Price: $ {findCourse?.price}</p>
 
 
-                <div className="mt-auto pt-4">
-                    <button onClick={() => dispatch(addItem({ id: findCourse?.id }))} className="cursor-pointer bg-[#07A698] text-white px-5 py-2 rounded-lg text-sm hover:bg-[#068a80] transition">
-                        Add to Cart
-                    </button>
+                    <div className="mt-auto pt-4">
+                        <button onClick={() => dispatch(addItem({ id: findCourse?.id }))} className="cursor-pointer bg-[#07A698] text-white px-5 py-2 rounded-lg text-sm hover:bg-[#068a80] transition">
+                            Add to Cart
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Home/LatestCourse.jsx
+++ src/components/Home/LatestCourse.jsx
@@ -53,73 +53,77 @@ const LatestCoursePage = () => {
                 </div>
 
                 {/* COURSE DATA */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                    {filteredCourses?.slice(0,3).map((c) => (
-                        <div data-aos="fade-up" data-aos-duration="1000" key={c?.id}>
-                            <div className="card w-80 h-[500] shadow-md rounded-2xl overflow-hidden bg-white hover:shadow-xl transition ">
-                                <figure className="h-48 w-full relative bg-gray-100 overflow-hidden">
-                                    <Image
-                                        src={c?.thumbnail}
-                                        alt={c?.title || 'image'}
-                                        width={500}
-                                        height={300}
-                                        className="object-cover w-full h-auto"
-                                    />
-                                </figure>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
+                    {
+                        filteredCourses.slice(0,3).map(c => (
+                            <div data-aos="fade-up" data-aos-duration="1000" key={c?.id}>
+                                <div className="card w-80 h-[495] shadow-md rounded-2xl overflow-hidden bg-white hover:shadow-xl transition ">
+                                    <figure className="h-48 w-full relative bg-gray-100 overflow-hidden">
+                                        <Image
+                                            src={c?.thumbnail}
+                                            alt={c?.title || 'image'}
+                                            width={500}
+                                            height={300}
+                                            className="object-cover w-full h-auto"
+                                        />
+                                    </figure>
 
-                                <div className="card-body h-52 p-4 flex flex-col gap-2 flex-1">
-                                    <p className="text-xs font-semibold text-[#07A698] bg-[#E6F6F4] rounded-full p-1 w-10 text-center mb-1">{c?.badge}</p>
+                                    <div className="card-body h-52 p-4 flex flex-col gap-2 flex-1">
+                                        <p className="text-xs font-semibold text-[#07A698] bg-[#E6F6F4] rounded-full p-1 w-10 text-center mb-1">{c?.badge}</p>
 
-                                    <h2 className="card-title text-lg font-bold leading-snug mb-1">
-                                        {c?.title}
-                                    </h2>
+                                        <h2 className="card-title text-lg font-bold leading-snug mb-1">
+                                            {c?.title}
+                                        </h2>
 
-                                    <div className="flex items-center gap-4 text-gray-500 text-sm leading-1">
-                                        <h1 className='text-gray-500 font-semibold'>Lessons: {c?.lessons}</h1>
-                                        <h1 className='text-gray-500 font-semibold'>Students: {c?.students}</h1>
-                                    </div>
-
-                                    <h1 className="text-sm text-gray-500 font-semibold mb-2">Views: {c?.views}</h1>
-
-                                    <div className="card-actions flex items-center gap-2 mb-3">
-                                        <div className="w-8 h-8 rounded-full overflow-hidden bg-gray-200">
-                                            {c?.instructor?.avatar ? (
-                                                <Image
-                                                    src={c.instructor.avatar}
-                                                    alt={c.instructor.name}
-                                                    width={32}
-                                                    height={32}
-                                                    className="object-cover"
-                                                />
-                                            ) : null}
+                                        <div className="flex items-center gap-4 text-gray-500 text-sm leading-1">
+                                            <h1 className='text-gray-500 font-semibold'>Lessons: {c?.lessons}</h1>
+                                            <h1 className='text-gray-500 font-semibold'>Students: {c?.students}</h1>
                                         </div>
-                                        <div>
-                                            <h1 className="font-medium text-xs">{c?.instructor?.name}</h1>
-                                            <h1 className="text-[10px] text-gray-500">{c?.instructor?.role}</h1>
+
+                                        <h1 className="text-sm text-gray-500 font-semibold mb-2">Views: {c?.views}</h1>
+
+                                        <div className="card-actions flex items-center gap-2 mb-3">
+                                            <div className="w-8 h-8 rounded-full overflow-hidden bg-gray-200">
+                                                {c?.instructor?.avatar ? (
+                                                    <Image
+                                                        src={c.instructor.avatar}
+                                                        alt={c.instructor.name}
+                                                        width={32}
+                                                        height={32}
+                                                        className="object-cover"
+                                                    />
+                                                ) : null}
+                                            </div>
+                                            <div>
+                                                <h1 className="font-medium text-xs">{c?.instructor?.name}</h1>
+                                                <h1 className="text-[10px] text-gray-500">{c?.instructor?.role}</h1>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div className="inline-block border-b border-gray-300 pb-1 text-sm mb-2">
-                                        ⭐⭐⭐⭐⭐⭐ {c?.rating}k
-                                    </div>
+                                        <div className="inline-block border-b border-gray-300 pb-1 text-sm mb-3">
+                                            ⭐⭐⭐⭐⭐⭐ {c?.rating}k
+                                        </div>
 
-                                    <div className="flex items-center justify-between text-sm">
-                                        <h1 className="font-semibold text-gray-800">Price: $ {c?.price}</h1>
-                                        <Link href={`/details/${c?.id}`}>
-                                            <button className="relative flex items-center gap-2 px-4 py-2 rounded-full font-semibold text-gray-800 overflow-hidden group border border-gray-300">
-                                                <span className="absolute inset-0 bg-linear-to-r from-[#39B8AD] to-[#2fa79b] scale-x-0 group-hover:scale-x-100 transform origin-left transition-transform duration-400 rounded-full z-0"></span>
+                                        <div className="flex items-center justify-between mt-auto text-sm">
+                                            <h1 className="font-semibold text-gray-800">Price: $ {c?.price}</h1>
+                                            <Link href={`/details/${c?.id}`}>
+                                                <button className="relative flex items-center cursor-pointer gap-2 px-4 py-2 rounded-full font-semibold text-gray-800 overflow-hidden group border border-gray-300">
+                                                    <span className="absolute inset-0 bg-linear-to-r from-[#39B8AD] to-[#2fa79b] scale-x-0 group-hover:scale-x-100 transform origin-left transition-transform duration-400 rounded-full z-0"></span>
 
-                                                <span className="relative z-10 flex items-center group-hover:text-white gap-2">
-                                                    Read More <FiArrowRight size={18} />
-                                                </span>
-                                            </button>
-                                        </Link>
+                                                    <span className="relative z-10 flex items-center group-hover:text-white gap-2">
+                                                        Read More <FiArrowRight size={18} />
+                                                    </span>
+                                                </button>
+                                            </Link>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))
+                    }
                 </div>
+
+
             </div>
         </section>
     );
